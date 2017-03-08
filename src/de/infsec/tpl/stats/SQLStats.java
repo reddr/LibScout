7bc4a03e3c03835c532aaeaca24417bdcb9f2f17
Erik Derr <derr@cs.uni-saarland.de>
2017-03-08 10:38:32 +0100

    removal of legacy table rows, added some new checks

--- src/de/infsec/tpl/stats/SQLStats.java
+++ src/de/infsec/tpl/stats/SQLStats.java
@@ -31,9 +31,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.infsec.tpl.TplCLI;
 import de.infsec.tpl.TplCLI.CliOptions;
-import de.infsec.tpl.hash.Hash;
 import de.infsec.tpl.hash.HashTree;
 import de.infsec.tpl.profile.LibProfile;
 import de.infsec.tpl.profile.SerializableProfileMatch;
@@ -54,9 +52,6 @@ public class SQLStats {
 	public static final String COL_LIB_CLASSES = "libClasses";
 	public static final String COL_LIB_METHODS = "libMethods";
 	public static final String COL_ROOT_PACKAGE = "rootPackage";
-	public static final String COL_HTREE_MIN_HASH = "htreeMinHash";
-	public static final String COL_HTREE_PUBONLY_HASH = "htreePubOnlyHash";
-	public static final String COL_HTREE_MAX_HASH  = "htreeMaxHash";
 	
 	// application table
 	public static final String T_APPLICATION = "apps";
@@ -69,14 +64,17 @@ public class SQLStats {
 	public static final String T_PROFILE = "profiles";
 	public static final String COL_LIBID = "libId";
 	public static final String COL_APPID = "appId";
-	public static final String COL_MATCHLEVEL = "matchLevel";
+	public static final String COL_MATCHLEVEL = "matchLevel";   // TODO: distinguish between full package match and full match due to partial matching
 	public static final String COL_ISOBFUSCATED = "isObfuscated";
 	public static final String COL_ROOTPCKG_PRESENT = "rootPckgPresent";
 	public static final String COL_SIMSCORE = "simScore";
-	public static final String COL_MATCH_CONF_MIN = "matchConfMin";
-	public static final String COL_MATCH_CONF_PUBONLY = "matchConfPubOnly";
-	public static final String COL_MATCH_CONF_MAX = "matchConfMax";
 
+	
+	// lib usage table (including API signatures)
+	public static final String T_LIBUSAGE = "libusage";
+	public static final String COL_PROFILEID = "profileId";
+	public static final String COL_API = "api";
+	
 	// lib package match table
 	public static final String T_PACKAGE_MATCH = "pckgmatch";
 	public static final String COL_LIBNAME = "libname";
@@ -92,6 +90,13 @@ public class SQLStats {
 		try {
 			logger.info("Generate DB from stats mode!");
 			logger.info(Utils.INDENT + "Loaded " + profiles.size() + " profiles from disk");
+
+			// don't update if db file exists
+	    	if (DB_FILE.exists()) {
+	    		logger.warn("DB file " + DB_FILE + " exists! -- No updates will be performed - Abort!");
+	    		return;
+	    	}
+			
 			List<SerializableAppStats> appStats = loadAppStats(CliOptions.statsDir);
 			generateDB(profiles, appStats);
 		} catch (Exception e) {
@@ -105,9 +110,10 @@ public class SQLStats {
 		// de-serialize app stats
 		long s = System.currentTimeMillis();
 		List<SerializableAppStats> appStats = new ArrayList<SerializableAppStats>();
+		
 		for (File f: Utils.collectFiles(dir, new String[]{"data"})) {
 			SerializableAppStats ap = (SerializableAppStats) Utils.disk2Object(f);
-			appStats.add(ap);
+			if (ap != null) appStats.add(ap);
 		}
 		
 		logger.info(Utils.INDENT + "Loaded " + appStats.size() + " app stats from disk (in " + Utils.millisecondsToFormattedTime(System.currentTimeMillis() - s) + ")");
@@ -126,31 +132,13 @@ public class SQLStats {
 			return;
 		}
 
-	    Connection connection = null;
-	    try {
-	    	File dbFile = DB_FILE; // TODO: make configurable
-	    	
-	    	if (dbFile.exists())
-	    		logger.warn("DB file " + DB_FILE + " exists! -- No updates will be performed - Abort!");
-	    	else {
-		    	// create a database connection
-				connection = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getName());
-				
-				createDB(connection);
-				updateDB(connection, profiles, appStats);
-	    	}
-
+    	// create a database connection
+	    try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE.getName())) {
+			createDB(connection);
+			updateDB(connection, profiles, appStats);
 	    } catch(SQLException e) {
 	    	logger.warn(Utils.stacktrace2Str(e));
-	    } finally {
-	    	try {
-		        if (connection != null)
-		          connection.close();
-	    	} catch (SQLException e) {
-	    		// connection close failed.
-		    	logger.warn(Utils.stacktrace2Str(e));
-	    	}
-	    }	
+	    }
 	}
 	
 	
@@ -171,9 +159,6 @@ public class SQLStats {
 			COL_LIB_CLASSES + " INTEGER not NULL, " +       // number of lib classes
 			COL_LIB_METHODS + " INTEGER, " +                // number of lib methods
 			COL_ROOT_PACKAGE + " VARCHAR(255), " +          // library root package, might be null if ambigious
-			COL_HTREE_MIN_HASH + " VARCHAR(255), " +        // hash tree root hash for config (true,true,true)
-			COL_HTREE_PUBONLY_HASH + " VARCHAR(255), " +    // hash tree root hash for config (false,true,false)
-			COL_HTREE_MAX_HASH + " VARCHAR(255), " +        // hash tree root hash for config (false,false,false)
 			"PRIMARY KEY (" + COL_NAME + ", " + COL_VERSION + ")"
 		+ ")";
 		stmt.executeUpdate(sql);
@@ -194,27 +179,32 @@ public class SQLStats {
 
 		// create profile match table
 		sql = "CREATE TABLE IF NOT EXISTS " + T_PROFILE +  "(" +
-			COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			COL_ID + " INTEGER PRIMARY KEY, " +                 
 			COL_LIBID + " INTEGER NOT NULL, " +               // Reference to T_LIBRARY.COL_ID
 			COL_APPID + " INTEGER NOT NULL, " +               // Reference to T_APPLICATION.COL_ID
 			COL_ISOBFUSCATED + " INTEGER NOT NULL, " +        // boolean, either 0 or 1
 			COL_ROOTPCKG_PRESENT  + " INTEGER NOT NULL, " +   // boolean, either 0 or 1
 			COL_MATCHLEVEL + " INTEGER NOT NULL, " +          // see SerializableProfileMatch.matchLevel
-			COL_SIMSCORE + " REAL, " +                        // similarity score if we have partial matching
-			COL_MATCH_CONF_MIN + " INTEGER, " +               // boolean, 0 or 1, if config (true,true,true) matches
-			COL_MATCH_CONF_PUBONLY + " INTEGER, " +           // boolean, 0 or 1, if config (false,true,false) matches
-			COL_MATCH_CONF_MAX + " INTEGER "                  // boolean, 0 or 1, if config (false,false,false) matches
+			COL_SIMSCORE + " REAL "                           // similarity score between [0..1]
 		+ ")";
 		stmt.executeUpdate(sql);
 		
 		// create naive package name match table
 		sql = "CREATE TABLE IF NOT EXISTS " + T_PACKAGE_MATCH +  "(" +
 			COL_APPID + " INTEGER NOT NULL, " +                 // Reference to T_APPLICATION.COL_ID
-			COL_LIBNAME + " VARCHAR(255) not NULL, " +          // library name
+			COL_LIBNAME + " VARCHAR(255) NOT NULL, " +          // library name
 			"PRIMARY KEY (" + COL_APPID + ", " + COL_LIBNAME + ")"
 		+ ")";
 		stmt.executeUpdate(sql);
-				
+
+		// create library api usage table
+		sql = "CREATE TABLE IF NOT EXISTS " + T_LIBUSAGE +  "(" +
+			COL_PROFILEID + " INTEGER NOT NULL, " +             // Reference to T_PROFILE.COL_ID
+			COL_API + " VARCHAR(255) NOT NULL, " +              // api signature
+			"PRIMARY KEY (" + COL_PROFILEID + ", " + COL_API + ")"
+		+ ")";
+		stmt.executeUpdate(sql);
+
 	}
 	
 	
@@ -223,10 +213,11 @@ public class SQLStats {
 
 		long starttime = System.currentTimeMillis();
 
-		final PreparedStatement ps_library = con.prepareStatement("INSERT INTO " + T_LIBRARY + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-		final PreparedStatement ps_app = con.prepareStatement("INSERT INTO " + T_APPLICATION + " VALUES (?,?,?,?,?,?,?,?)");
-		final PreparedStatement ps_profile = con.prepareStatement("INSERT INTO " + T_PROFILE + " VALUES (?,?,?,?,?,?,?,?,?,?)");
+		final PreparedStatement ps_library = con.prepareStatement("INSERT INTO " + T_LIBRARY + " VALUES (?,?,?,?,?,?,?,?,?)");
+		final PreparedStatement ps_app = con.prepareStatement("INSERT OR IGNORE INTO " + T_APPLICATION + " VALUES (?,?,?,?,?,?,?,?)");
+		final PreparedStatement ps_profile = con.prepareStatement("INSERT INTO " + T_PROFILE + " VALUES (?,?,?,?,?,?,?)");
 		final PreparedStatement ps_pckgMatch = con.prepareStatement("INSERT INTO " + T_PACKAGE_MATCH + " VALUES (?,?)");
+		final PreparedStatement ps_libUsage = con.prepareStatement("INSERT INTO " + T_LIBUSAGE + " VALUES (?,?)");
 		
 		// add all library profiles
 		HashMap<Pair<String,String>, Integer> profile2ID = new HashMap<Pair<String,String>, Integer>();
@@ -247,28 +238,30 @@ public class SQLStats {
 					ps_library.setInt(8, htree.getNumberOfMethods());
 			
 			ps_library.setString(9, lib.packageTree.getRootPackage());
-			
-			HashTree tree = HashTree.getTreeByConfig(lib.hashTrees, true, true, true);
-			ps_library.setString(10, tree == null? "" : Hash.hash2Str(tree.getRootHash()));
-
-			tree = HashTree.getTreeByConfig(lib.hashTrees, false, true, false);
-			ps_library.setString(11, tree == null? "" : Hash.hash2Str(tree.getRootHash()));
-			
-			tree = HashTree.getTreeByConfig(lib.hashTrees, false, false, false);
-			ps_library.setString(12, tree == null? "" : Hash.hash2Str(tree.getRootHash()));
-
-			ps_library.execute();
+			ps_library.addBatch();
 			
 			profile2ID.put(lib.getLibIdentifier(), i+1);
-			logger.info(Utils.INDENT + "- Added library (" + (i+1) + "/" + profiles.size() + "): " + lib.getLibIdentifier());
+			//logger.info(Utils.INDENT + "- Added library (" + (i+1) + "/" + profiles.size() + "): " + lib.getLibIdentifier());
 		}
+		
+		if (profiles.isEmpty())
+			throw new SQLException("No libraries to add!");
+		
+		ps_library.executeBatch();
+		logger.info(Utils.INDENT + "- Added libraries");
 
 		
 		// update app / profile match table
+		int profileId = 0;
+		
 		for (int i = 0; i < stats.size(); i++) {
 			SerializableAppStats appStat = stats.get(i);
-			int appId = i+1;
 			
+			// skip apps with no library detected 
+			// TODO: && appStat.packageMatches.isEmpty()
+			if (appStat.pMatches.isEmpty()) continue;
+			
+			int appId = i+1;
 			ps_app.setInt(1, appId);
 			ps_app.setString(2, appStat.manifest.getPackageName());
 			ps_app.setInt(3, appStat.manifest.getVersionCode());
@@ -277,12 +270,10 @@ public class SQLStats {
 			ps_app.setInt(6, appStat.appPackageCount);
 			ps_app.setInt(7, appStat.appClassCount);
 			
-//			logger.debug(LogConfig.INDENT + "- add app from " + appStat.appFileName + " : " + appStat.manifest.getPackageName() + "  (version: " + appStat.manifest.getVersionCode() + ")");
-			ps_app.execute();
+			//logger.info(Utils.INDENT + "- add app from " + appStat.appFileName + " : " + appStat.manifest.getPackageName() + "  (version: " + appStat.manifest.getVersionCode() + ")");
+			ps_app.addBatch();
 
-			
 			/* update profile matches */
-			
 			// get unique libnames and the max simScore
 			Map<String, Float> uniqueLibraries = new HashMap<String,Float>();
 			for (SerializableProfileMatch pm: appStat.pMatches) {
@@ -291,44 +282,57 @@ public class SQLStats {
 			}
 			
 			Set<String> matchedLibs = new HashSet<String>();
-			
 			for (SerializableProfileMatch pm: appStat.pMatches) {
 				if (pm.matchLevel < SerializableProfileMatch.MATCH_ALL_CONFIGS && pm.simScore < uniqueLibraries.get(pm.libName)) continue;  // only export matches with highest sim Score
 				matchedLibs.add(pm.libName);
+				profileId++; // increment global profile id counter
+			
+				if (!profile2ID.containsKey(pm.getLibIdentifier()))
+					continue;   // if we do not have a lib identifier, continue;
 				
 				int libId = profile2ID.get(pm.getLibIdentifier());
 								
+				ps_profile.setInt(1, profileId);
 				ps_profile.setInt(2, libId);
 				ps_profile.setInt(3, appId);
 				ps_profile.setBoolean(4, pm.isLibObfuscated);
 				ps_profile.setInt(5, pm.libRootPackagePresent? 1 : 0);
 				ps_profile.setInt(6, pm.matchLevel);
 				ps_profile.setFloat(7, pm.matchLevel == SerializableProfileMatch.MATCH_ALL_CONFIGS? 1f : pm.simScore);
+				ps_profile.addBatch();
 				
-				 // did configs match?
-				int[] cfg = new int[]{0, 0, 0};  // TODO deprecated
-
-				ps_profile.setInt(8, cfg[0]);
-				ps_profile.setInt(9, cfg[1]);
-				ps_profile.setInt(10, cfg[2]);
-				
-				ps_profile.execute();
-			}
-			
-			// update package name matches
-			Set<String> alreadyAdded = new HashSet<String>();
-			for (String lib: appStat.packageMatches) {
-				   // we currently exclude play services and Guava (com.google), as the auto root package name detection does not work properly
-				if (!matchedLibs.contains(lib) && !lib.startsWith("Google Play Services") && !lib.startsWith("Guava") && alreadyAdded.add(lib)) {
-					ps_pckgMatch.setInt(1, appId);
-					ps_pckgMatch.setString(2, lib);
-					ps_pckgMatch.execute();
+				// API usage
+				if (!pm.usedLibMethods.isEmpty()) {
+					for (String sig: pm.usedLibMethods) {   // foreach api signature
+						ps_libUsage.setInt(1, profileId);
+						ps_libUsage.setString(2, sig);
+						ps_libUsage.addBatch();
+					}
+					ps_libUsage.executeBatch();
 				}
 			}
 			
-			logger.info(Utils.INDENT + "- Added app (" + appId + "/" + stats.size() + "): " + appStat.manifest.getPackageName() + " (" + appStat.manifest.getVersionCode() + ")");
+			if (i > 0 && (i % 1000 == 0 || i == stats.size()-1)) {
+				ps_app.executeBatch();
+				ps_profile.executeBatch();
+				logger.info("## App/Profile batch " + i + "/" + stats.size() + "  executed");
+			}			
+			
+// TODO enable again			
+//			// update package name matches
+//			Set<String> alreadyAdded = new HashSet<String>();
+//			for (String lib: appStat.packageMatches) {
+//				   // we currently exclude play services and Guava (com.google), as the auto root package name detection does not work properly
+//				if (!matchedLibs.contains(lib) && !lib.startsWith("Google Play Services") && !lib.startsWith("Guava") && alreadyAdded.add(lib)) {
+//					ps_pckgMatch.setInt(1, appId);
+//					ps_pckgMatch.setString(2, lib);
+//					ps_pckgMatch.execute();
+//				}
+//			}
+			
+//TODO			logger.info(Utils.INDENT + "- Added app (" + appId + "/" + stats.size() + "): " + appStat.manifest.getPackageName() + " (" + appStat.manifest.getVersionCode() + ")");
 		}
-
+		
 		logger.info("DB Update (" + profiles.size() + " lib profiles, " + stats.size() + " app stats) done in " + Utils.millisecondsToFormattedTime(System.currentTimeMillis() - starttime));
 	}
 }
